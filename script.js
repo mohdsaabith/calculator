6f068b1f433aba0dbd149500ca6f34961bc1aa76
Mohammed Saabith Cp <44156514+mohdsaabith@users.noreply.github.com>
2022-08-31 11:41:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function valueButton(s){
+    calculatorForm.screen.value += s.value;
+}
+
+function Delete(){
+    calculatorForm.screen.value = null;
+}
+
+function Calculate(){
+    calculatorForm.screen.value = eval(calculatorForm.screen.value)
+}
